929c4b11e65cbe8523849b0f5495edda9152bea1
999MeshDi666 <havkpzz@gmail.com>
2022-08-05 11:28:46 +0600

    fix redirect to room after game function

--- src/content/Winners/index.jsx
+++ src/content/Winners/index.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import { useParams, useNavigate } from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import {fbaseDB} from '../../utils/firebase-config'
-import { ref, onValue, set, orderByChild, query, update, child} from "firebase/database";
+import { ref, onValue, set, orderByChild, query, update, child, remove} from "firebase/database";
 
 
 import SoundBtn  from "../SoundController/index";
@@ -12,20 +12,44 @@ const WinnersPage = ({soundPlaying}) =>{
 
     const {roomIDFromUrl} = useParams();
     const {gameIDFromUrl} = useParams();
-    const navigateToRoom = useNavigate();
+    const navigateResetToRoom = useNavigate();
     const [isPlaying, setIsPlaying] = useState()
+    const [users, setUsers] = useState();
     const userID = JSON.parse(sessionStorage.getItem('current-user'))['uid']
     const usersDataRef =  query(ref(fbaseDB, `polyglot/rooms/${roomIDFromUrl.substring(1)}/users/`), orderByChild('createdAt'))
 
     
+    
     const handleRedirectToRoom = () =>{
         const resetScore = ref(fbaseDB, `polyglot/rooms/${roomIDFromUrl.substring(1)}/users/${userID}/`);
         update(resetScore, {score: 0})
-
         const updateUsersPath = query(ref(fbaseDB, `polyglot/rooms/${roomIDFromUrl.substring(1)}/current-path/`), orderByChild('createdAt'))
         set(updateUsersPath,{userPath: ''})    
-        navigateToRoom(`/room/:${roomIDFromUrl.substring(1)}`);
     }
+
+    useEffect(()=>{
+        const redirectToRoom = query(ref(fbaseDB, `polyglot/rooms/${roomIDFromUrl.substring(1)}/current-path/userPath/`), orderByChild('createdAt'));
+        onValue(redirectToRoom, (snapshot)=>{
+            if(snapshot.val() === ''){
+                navigateResetToRoom(`/room/:${roomIDFromUrl.substring(1)}`);
+            }
+        })
+    },[])
+
+    //get users data
+    useEffect(()=>{
+        onValue(query(ref(fbaseDB, `polyglot/rooms/${roomIDFromUrl.substring(1)}/users/${userID}/score/`), orderByChild('score')), (snapshot) => {
+            const userList = []
+            snapshot.forEach((child) =>{
+                userList.push(child.val())
+            })
+            setUsers(userList)
+        });
+    },[])
+
+   
+
+    
 
     //get users playing state
     useEffect(()=>{
